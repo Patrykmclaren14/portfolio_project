2dfe103a77fda39c99ca28e2c6df1a7e363247d8
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-14 19:34:30 +0100

    add Navbar to the Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './Header.styles.scss'
+import { Navbar } from "../Navbar";
 
 export const Header = () => {
   return (
     <header className="header">
-
+      <Navbar />
     </header>
   )
 }
