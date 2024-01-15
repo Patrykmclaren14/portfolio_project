4433ccc80603e9601768e469686b6fa88f6bb47e
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-15 19:34:29 +0100

    Implement Hero component to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './Header.styles.scss'
-import Navbar from "../Navbar/Navbar";
+import {Navbar} from "../Navbar/Navbar";
+import {Hero} from "../Hero/Hero";
 
 export const Header = () => {
   return (
     <header className="header">
       <Navbar />
+      <Hero />
     </header>
   )
 }
-
+//  
 export default Header;
