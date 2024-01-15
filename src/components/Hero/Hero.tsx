d0f5bd96f1c0f339f3b68eea7c3559c92561f8b0
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-15 20:40:43 +0100

    Set Up Environment Configuration and Enhance Components

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -18,7 +18,7 @@ export const Hero = () => {
     <div className="hero">
       <h1 className="gradient-text hero__headline">welcome</h1>
       <h1 className="hero__title name ">Patryk Wierzbowski</h1>
-      {isNameLoading && <h1 className="hero__title proffession"><span className="gradient-text">I'm</span> Full Stack Developer</h1>}
+      {isNameLoading && <h1 className="hero__title proffession"><span className="gradient-text">I&apos;m</span> Full Stack Developer</h1>}
       {isProffession && <button className="button hero__button">Get to know me</button>}
       <div className="cosmonaut"></div>
     </div>
