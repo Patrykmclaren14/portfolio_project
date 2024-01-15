794057804753260416164684f9460aa4c9b48f7b
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-15 20:07:27 +0100

    Update Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import './Hero.styles.scss'
+import '../../App.styles.scss'
 
 export const Hero = () => {
   const [isNameLoading, setIsNameLoading] = useState(false);
@@ -15,9 +16,9 @@ export const Hero = () => {
 
   return (
     <div className="hero">
-      <h1 className="hero__headline">welcome</h1>
-      <h1 className="hero__title name">Patryk Wierzbowski</h1>
-      {isNameLoading && <h1 className="hero__title proffession">Full Stack Developer</h1>}
+      <h1 className="gradient-text hero__headline">welcome</h1>
+      <h1 className="hero__title name ">Patryk Wierzbowski</h1>
+      {isNameLoading && <h1 className="hero__title proffession"><span className="gradient-text">I'm</span> Full Stack Developer</h1>}
       {isProffession && <button className="button hero__button">Get to know me</button>}
       <div className="cosmonaut"></div>
     </div>
