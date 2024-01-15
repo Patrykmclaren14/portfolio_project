576516e849691c8d6470c23f20572091dd7f25b0
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-15 21:55:12 +0100

    Implement TechStack in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import './App.styles.scss'
-import {Header} from './components/Header'
 import './index.css'
+import { Header } from './components/Header'
 import { AboutMe } from "./components/AboutMe";
+import { TechStack } from "./components/TechStack";
 
 function App() {
   return (
     <div className="home">
       <Header />
       <AboutMe />
+      <TechStack />
     </div>
   );
 }
